f13c5d2ed1117d4dbb84430a01177a678811d0a3
JunyuKuang <lightscreen.app@gmail.com>
2023-08-26 10:29:31 +0800

    Done

--- SleepNowHotKey/AppDelegate.m
+++ SleepNowHotKey/AppDelegate.m
@@ -6,6 +6,22 @@
 //
 
 #import "AppDelegate.h"
+@import Carbon;
+@import ServiceManagement;
+
+static void sleepNow(void)
+{
+    [NSTask launchedTaskWithExecutableURL:[NSURL fileURLWithPath:@"/bin/sh"]
+                                arguments:@[@"-c", @"pmset sleepnow"]
+                                    error:nil
+                       terminationHandler:nil];
+}
+
+static OSStatus HotKeyEventCallback(EventHandlerCallRef _, EventRef event, void *context)
+{
+    sleepNow();
+    return noErr;
+}
 
 @interface AppDelegate ()
 
@@ -15,14 +31,42 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    CGRequestPostEventAccess();
+    [self configureHotKeys];
+    [self configureLaunchAtLogin];
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (BOOL)applicationShouldHandleReopen:(NSApplication *)sender hasVisibleWindows:(BOOL)flag
+{
+    sleepNow();
+    return NO;
 }
 
+- (void)configureHotKeys
+{
+    EventTargetRef target = GetApplicationEventTarget();
+    EventTypeSpec spec = {
+        .eventClass = kEventClassKeyboard,
+        .eventKind = kEventHotKeyPressed
+    };
+    InstallEventHandler(target, HotKeyEventCallback, 1, &spec, nil, nil);
+    
+    EventHotKeyRef hotKeyRef;
+    
+    EventHotKeyID hotKeyID = {
+        .signature = 'JONY',
+        .id = 1
+    };
+    RegisterEventHotKey(kVK_ANSI_Q, optionKey, hotKeyID, target, 0, &hotKeyRef);
+    
+//    hotKeyID.id = 2;
+//    RegisterEventHotKey(kVK_ANSI_S, optionKey, hotKeyID, target, 0, &hotKeyRef);
+}
+
+- (void)configureLaunchAtLogin
+{
+    [SMAppService.mainAppService registerAndReturnError: nil];
+}
 
 - (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
     return YES;
